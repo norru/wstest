a5f81bf18e7b729d4539c8621ca701f004a573d3
Nicola Orrù <nigu.orru@gmail.com>
2012-02-26 22:46:57 +0000

    Removed references to webmachine

--- apps/wstest/src/wstest_sup.erl
+++ apps/wstest/src/wstest_sup.erl
@@ -11,12 +11,6 @@ start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
-    Webmachine = {
-		webmachine_mochiweb,
-		{webmachine_mochiweb, start, [load_webmachine_config()]},
-		permanent, 5000, worker, [webmachine_mochiweb]
-	},
-
 	MochiOptions = [{ip, "0.0.0.0"}, {port, 8001}],
 	
     Mochionly = {
@@ -25,25 +19,4 @@ init([]) ->
 		permanent, 5000, worker, [wstest_web]
 	},
 	
-    {ok, {{one_for_one, 5, 10}, [Mochionly, Webmachine]}}.
-
-load_webmachine_config() ->
-    
-    Ip = get_app_env(web_ip, "0.0.0.0"),
-    Port = get_app_env(webmachine_port, 8000),
-    LogDir = get_app_env(log_dir, "priv/log"),
-
-    {ok, Dispatch} = file:consult(filename:join(code:priv_dir(wstest), "dispatch.conf")),
-
-    [
-		{ip, Ip},
-		{port, Port},
-		{log_dir, LogDir},
-		{dispatch, Dispatch}
-	].
-
-get_app_env(Env, Default) ->
-	case application:get_env(wstest, Env) of
-		{ok, Val} -> Val;
-		undefined -> Default
-	end.
+    {ok, {{one_for_one, 5, 10}, [Mochionly]}}.
--- apps/wstest/src/wstest_web.erl
+++ apps/wstest/src/wstest_web.erl
@@ -1,26 +1,27 @@
 -module(wstest_web).
-
 -export([start/1, stop/0, handler/1]).
 
 start(Options) ->
 	io:format("Options=~p~n", [Options]),
-
-	Status = mochiweb_http:start([
-								  {name, ?MODULE},
-								  {loop, fun handler/1} |
-								  Options
-								 ]
-								),
-	Status.
+	mochiweb_http:start([
+						 {name, wstest_web},
+						 {loop, fun handler/1} |
+							 Options
+						]
+					   ).
 
 stop() ->
 	io:format("Stop!~n"),
-    mochiweb_http:stop(?MODULE).
+	mochiweb_http:stop(wstest_web).
 
 handler(Req) ->
-    "/" ++ Path = Req:get(path),
-    case Req:get(method) of
-        _ ->
-			io:format("Path=~p, Req=~p~n", [Path, Req]),
-            Req:respond({501, [], []})
-    end.
+	"/" ++ Path = Req:get(path),
+	io:format("Path=~p, Req=~p~n", [Path, Req]),
+	case Req:get(method) of
+		'POST'->
+			Req:respond({501,[],[]});
+		'GET' ->
+			Req:respond({200, [{"Content-Type", "text/html"}], ["<body>Hello, world! " ++ Path ++ "</body>"]});
+		_ ->
+			Req:not_found()
+	end.
