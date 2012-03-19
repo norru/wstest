23689ebb1c252039624ea0b027c25dc335913e46
Nicola Orrù <nigu.orru@gmail.com>
2012-03-19 22:48:11 +0000

    Implemented lookup table for mime type

--- apps/wstest/src/wstest_web.erl
+++ apps/wstest/src/wstest_web.erl
@@ -1,6 +1,9 @@
 -module(wstest_web).
 -export([start/1, stop/0, handler/1]).
 
+mime_types() ->
+	[{"application/xml", xml}, {"text/xml", xml}].
+
 start(Options) ->
 	io:format("Options=~p~n", [Options]),
 	mochiweb_http:start([
@@ -17,14 +20,37 @@ stop() ->
 xml_body(_) ->
   "<?xml version=\"1.0\"?><hello/>".
 
-process(Req, xml, 'GET', "test") ->
-	Req:respond({200, [{"Content-Type", "text/xml"}], xml_body(Req)});
-process(Req, xml, 'POST', _) ->
-	Req:respond({501,[],[]});
-process(Req, _, _, _)->
+process(Req, _, xml, 'GET', "test") ->
+	Req:ok({"application/xml", xml_body(Req)});
+process(Req, "application/xml", xml, 'POST', _) ->
+	Req:respond({201,[],[]});
+process(Req, _, _, _, _)->
 	Req:not_found().
+
+produces(Req) ->
+	produces(Req, mime_types()).
+produces(_,[]) ->
+	[];
+produces(Req,[{MimeType, ContentType} | T]) ->
+	case Req:accepts_content_type(ContentType) of
+		true -> MimeType;
+		_ -> produces(Req, T)
+	end.
+
+consumes(Req) ->
+	consumes(Req:get_primary_header_value("content-type"), mime_types()).
+consumes(_, []) ->
+	[];
+consumes(GotMimeType, [{MimeType, ContentType} | T]) ->
+	case GotMimeType of
+		MimeType -> ContentType;
+		_ -> consumes(GotMimeType, T)
+	end.
 
 handler(Req) ->
 	"/" ++ Path = Req:get(path),
 	io:format("Path=~p, Req=~p~n", [Path, Req]),
-	process(Req, xml, Req:get(method), Path).
+	Consumes = consumes(Req),
+	Produces = produces(Req),
+	Method = Req:get(method),
+	process(Req, Consumes, Produces, Method, Path).
