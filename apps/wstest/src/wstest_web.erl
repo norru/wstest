ce0bf13a0656eaefbe6e68773c5167738ba787fb
Nicola Orrù <nicola_orru@scee.net>
2012-05-11 21:32:34 +0100

    Working EventStream with browser

--- apps/wstest/src/wstest_web.erl
+++ apps/wstest/src/wstest_web.erl
@@ -2,31 +2,38 @@
 -export([start/1, stop/0, handler/1, start_greet/0, process/5]).
 
 mime_types() ->
-	[{"text/plain", text}, {"application/xml", xml}, {"text/xml", xml}].
+	[{"text/plain", text}, {"application/xml", xml}, {"text/xml", xml}, {"text/html", html}].
 
 start(Options) ->
-	io:format("Options=~p~n", [Options]),
+	error_logger:logfile({open, "wstest.log"}),
+	log(info, "Options=~p~n", [Options]),
 	register(dispatcher, spawn_link(fun dispatcher/0)),
 	mochiweb_http:start([{name, wstest_web}, {loop, fun handler/1} | Options]).
 
 stop() ->
-	io:format("Stop!~n"),
+	log(info, "Stop!~n"),
 	mochiweb_http:stop(wstest_web).
 
-start_greet() -> spawn(fun() -> greet("Ciao!\n") end).
-greet(Message) ->
+log(Level, Format) -> log(Level, Format, []).
+log(info, Format, Data) -> error_logger:info_msg(Format, Data);
+log(warn, Format, Data) -> error_logger:warning_msg(Format, Data);
+log(_, Format, Data) -> error_logger:error_msg(Format, Data).
+
+start_greet() -> spawn(fun() -> greet("Ciao!", 200) end).
+
+greet(Message, Timeout) ->
 	receive
-		Newmessage -> greet(Newmessage)
-	after 1000 ->
+		Newmessage -> greet(Newmessage, Timeout)
+	after Timeout ->
 		dispatcher ! {broadcast, Message},
-		greet(Message)
+		greet(Message, Timeout)
 	end.
 
 dispatcher() ->
 	dispatcher([], 0).
 
 dispatcher(Handlers, Counter) ->
-	io:format("Dispatcher invoked~n~n"),
+	log(info, "Dispatcher invoked~n~n"),
 	receive
 		{broadcast, Message} -> broadcast(Handlers, io_lib:format("~s~p\n", [Message, Counter])), dispatcher(Handlers, Counter + 1);
 		{addhandler, Handler} -> dispatcher([Handler|Handlers], Counter);
@@ -45,19 +52,19 @@ broadcast([], _) ->
 	ok.
 
 send_chunks(Req, Resp, Tail) ->
-	io:format("Adding: ~p~n", [self()]),
+	log(info, "Adding: ~p~n", [self()]),
 	dispatcher ! {addhandler, self()},
 	send_chunks(Req, Resp, Tail, 100),
 	Resp.
 
 send_chunks(_Req, Resp, Tail, 0) ->
-	Resp:write_chunk(Tail),	
+	send_event(Resp, 'end', Tail),
 	Resp:write_chunk(<<>>),
 	dispatcher ! {removehandler, self()},
-	io:format("Removing: ~p~n", [self()]);
+	log(info, "Removing: ~p~n", [self()]);
 send_chunks(Req, Resp, Tail, Count) ->
 	receive
-		{echo, Message} -> Resp:write_chunk(Message)
+		{echo, Message} -> send_event(Resp, data, Message)
 	end,
 	send_chunks(Req, Resp, Tail, Count -1).
 
@@ -68,18 +75,24 @@ receive_chunks({_Size, Bin}, State) ->
 	dispatcher ! {broadcast, Bin},
 	[Bin | State].
 
-process(Req, _, xml, 'GET', "test") ->
-	Resp = Req:ok({"application/xml", [], chunked}),
-	Resp:write_chunk("<?xml version=\"1.0\"?>\n"),
-	Resp:write_chunk("<hello>\n"),
-	send_chunks(Req, Resp, "</hello\n>");
-process(Req, _, text, 'GET', "test") ->
-	Resp = Req:ok({"text/plain", [], chunked}),
-	Resp:write_chunk("Hello!\n"),
+send_event(Resp, Type, Message) ->
+	Resp:write_chunk(io_lib:format("~p: ~s~n~n", [Type, Message])).
+  
+process(Req, _, _, 'GET', "feed") ->
+	Resp = Req:ok({"text/event-stream", [], chunked}),
+	send_event(Resp, data, "Hello!"),
 	send_chunks(Req, Resp, "Bye\n");
 process(Req, _, _, 'POST', "test") ->
 	receive_chunks(Req),
 	Req:ok({"text/plain", "Sent"});
+process(Req, _, _, 'GET', Page) ->
+	case file:read_file(Page) of
+		{ok,FileBin} ->
+			Req:ok({"text/html", [{"Content-Disposition", "inline"}], FileBin});
+		_ ->
+			log(error, "Not found: ~p", [Page]),
+			Req:not_found()
+	end;
 process(Req, _, _, _, _)->
 	Req:not_found().
 
@@ -105,7 +118,7 @@ consumes(GotMimeType, [{MimeType, ContentType} | T]) ->
 
 handler(Req) ->
 	"/" ++ Path = Req:get(path),
-	io:format("Path=~p, Req=~p~n", [Path, Req]),
+	log(info, "Path=~p, Req=~p~n", [Path, Req]),
 	Consumes = consumes(Req),
 	Produces = produces(Req),
 	Method = Req:get(method),
